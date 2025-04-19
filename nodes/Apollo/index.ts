a246cd56ae897bec7f31ebbde8449e17732717e0
Tayo Onabule <57198518+tayoonabule@users.noreply.github.com>
2025-04-19 15:04:40 +0100

    clean

--- nodes/Apollo/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Entry point for custom n8n nodes for npm publishing.
- * Exports an array of all node classes to be loaded by n8n.
- */
-import { ApolloIo } from './ApolloIo.node';
-export const nodes = [ApolloIo];
